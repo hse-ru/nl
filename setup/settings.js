455f0bc7acd368a31c2235b966aba463fc08435a
Anna Laurinavichyute <annlaurin@gmail.com>
2014-04-24 17:43:49 +0400

    Used materials from anothe tutorial.ejs

---
+++ setup/settings.js
@@ -0,0 +1,48 @@
+var Experigen =  {
+	settings: {
+
+        // SETUP: Enter Experiment Name
+		experimentName: "EnterExperimentNameHere", // use only A-Z, a-z, 0-9
+		
+		databaseServer: "http://db.phonologist.org/",
+
+		online: true,
+		
+		strings: {
+		    // SETUP: Enter Title for Browser Window
+			windowTitle:     "Enter Title Here",
+			connecting:		 "Connecting...",
+			loading:         "Loading...",
+			soundButton:     "    ►    ",
+			continueButton:  "   continue   ",
+			errorMessage:    "An error occurred. We apologize for the inconvenience.",
+			emptyBoxMessage: "Please supply your answer in the text box."
+		},
+		
+		audio: true,
+		
+		recordResponseTimes: true,
+		
+		progressbar: {
+			visible: true, 
+			adjustWidth: 6,
+			percentage: false
+		},
+		
+		items: "resources/index.txt",
+		
+		otherresources: {	
+		},
+
+		folders: {
+			views: "views/",
+			sounds: "resources/sounds/",
+			pictures: "resources/pictures/"
+		},
+	
+		footer: "views/footer.html",
+		missingview: "views/missingview.ejs"
+	}	
+};
+
+
