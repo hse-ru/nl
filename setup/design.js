db5585a75c1605e6da5d8c1c2d5dfdbfed1e3025
Anna Laurinavichyute <annlaurin@gmail.com>
2014-04-28 19:52:59 +0400

    Trying to change it. again

--- setup/design.js
+++ setup/design.js
@@ -38,6 +38,7 @@ Experigen.initialize = function () {
     tests[0].view = "warmup.ejs";
 
 	this.addStaticScreen("intro.ejs");
+	this.addStaticScreen("getgoing.ejs")
 	this.addBlock(tests);
 	this.addStaticScreen("demographic.ejs");
 	this.addStaticScreen("finalthanks.ejs");
