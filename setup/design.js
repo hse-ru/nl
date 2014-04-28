04de354fdef1e2e299f123e4a2d7bace57812318
Anna Laurinavichyute <annlaurin@gmail.com>
2014-04-28 19:50:56 +0400

    Trying to change it. again

--- setup/design.js
+++ setup/design.js
@@ -1,18 +1,41 @@
 Experigen.initialize = function () {
 	
-    var tests = [];
-	var num = this.resource("num");
-	var test_length = num.length;
-	
-
-	for(var i = 3; i <= test_length; i += 1) {
-	    tests = tests.concat(num.subset("num",i.toString()));
+	var conditionList = function (tests, condition) {
+	    var conditions = [];
+	    
+	    for(var i = 0; i < tests.length; i++) {
+	        if(!conditions.contains(tests[i][condition])) {
+	            conditions.push(tests[i][condition]);
+	        }
+	    }
+	    return conditions;
 	}
+
+    var tests = [];
+	
+	var items = this.resource("items");
+	
+	// Separate tests from fillers
+	var all_tests = items.exclude("condition","filler");
+	var fillers = items.subset("condition","filler");
+	
+	var latin_square_row = conditionList(all_tests,"condition");
+	var num_conditions = latin_square_row.length;
+	
+	
+	latin_square_row = latin_square_row.shuffle();
+	
+	var test_length = all_tests.length/num_conditions;
+	
+	// Apply Latin Square selection to each question
+	for(var i = 1; i <= test_length; i += 1) {
+	    tests = tests.concat(all_tests.subset("item",i.toString()).subset("condition",latin_square_row[(i % num_conditions)]));
+	}
+	
+	tests = tests.concat(fillers);
 	
 	tests = tests.pairWith("view","stimulus.ejs").shuffle();
     tests[0].view = "warmup.ejs";
-	tests[1].view = "warmup.ejs";
-	tests[2].view = "warmup.ejs";
 
 	this.addStaticScreen("intro.ejs");
 	this.addBlock(tests);
