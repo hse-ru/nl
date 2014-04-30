1afccba2c2d758aa06d9f6e303015e57ebadf759
Anna Laurinavichyute <annlaurin@gmail.com>
2014-04-30 22:03:21 +0400

    inserted new warmup.js

--- setup/design.js
+++ setup/design.js
@@ -1,14 +1,12 @@
 Experigen.initialize = function () {
 	
     var tests = [];
-	var items = this.resource("items");
-//	var test_length = items.length;
-	
+	var items = this.resource("items");	
 	
 	tests = items.pairWith("view","stimulus.ejs");
-//    tests[0].view = "warmup.ejs";
 
 	this.addStaticScreen("intro.ejs");
+	this.addStaticScreen("warmup.ejs");
 	this.addStaticScreen("getgoing.ejs")
 	this.addBlock(tests);
 	this.addStaticScreen("demographic.ejs");
