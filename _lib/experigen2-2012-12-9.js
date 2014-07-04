b96ea948404fb05cb8bc6e8669a365ff43dbd160
Anna Laurinavichyute <annlaurin@gmail.com>
2014-07-04 22:01:44 +0400

    Warmup & Intro

--- _lib/experigen2-2012-12-9.js
+++ _lib/experigen2-2012-12-9.js
@@ -497,9 +497,9 @@ var timer_maker = function (  ) {
 						Experigen.screen().advance();
 					} else {
 						soundManager.play(soundID2);
-						Experigen.screen().advance();
-// setTimeout(function(){Experigen.screen().advance()}, 3000);	
-// wait for the slowpokes. Why does it not work?
+//						Experigen.screen().advance();
+						setTimeout(function(){Experigen.screen().advance()}, 3000);	
+						// wait for the slowpokes. Why does it not work?
 						
 					}
 				}
