7f457011a9d612408866e8e2b6fedb4847a84605
Anna Laurinavichyute <annlaurin@gmail.com>
2014-07-04 17:30:10 +0400

    Warmup & Intro

--- _lib/experigen2-2012-12-9.js
+++ _lib/experigen2-2012-12-9.js
@@ -506,7 +506,7 @@ var timer_maker = function (  ) {
 		str += '<input type="button" ';
 		str += ' id="' + soundID +'"';
 		str += ' value="' + label + '"';
-	//	str += ' onClick="Experigen.screen().playSound(\'' + soundID + '\',this);"'
+		str += ' onClick="Experigen.screen().playSound(\'' + soundID + '\',this);"'
 		str += ' class="soundbutton"'
 		str += '>';
 		return str;
