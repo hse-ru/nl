1c4f31a2c3bc601636a1047eeae08558b97a3bb4
Anna Laurinavichyute <annlaurin@gmail.com>
2014-07-11 22:11:59 +0400

    Deleting 'advance' on the shitty button

--- _lib/experigen2-2012-12-9.js
+++ _lib/experigen2-2012-12-9.js
@@ -522,7 +522,7 @@ function hide(obj) {
 		str += '<input type="button" ';
 		str += ' id="' + soundID +'"';
 		str += ' value="' + label + '"';
-		str += ' onClick="Experigen.screen().playSound(\'' + soundID + '\',this);" '; 
+		str += ' onClick="Experigen.screen().playSound(\'' + soundID + '\',this); this.style.display = 'none';" '; 
 		str += ' class="soundbutton"';
 		str += '>';
 		return str;
