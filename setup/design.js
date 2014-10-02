cc76d745795f931638a7226ce33b569d465ea54b
Anna Laurinavichyute <annlaurin@gmail.com>
2014-10-02 12:36:50 +0400

    fixing the flow

--- setup/design.js
+++ setup/design.js
@@ -6,9 +6,9 @@ Experigen.initialize = function () {
 	tests = items.pairWith("view","stimulus.ejs");
 
 	this.addStaticScreen("intro.ejs");
-//	this.addStaticScreen("concent.ejs");
-	this.addStaticScreen("warmup.ejs");
-	this.addStaticScreen("warmup1.ejs");
+	this.addStaticScreen("concent.ejs");
+//	this.addStaticScreen("warmup.ejs");
+//	this.addStaticScreen("warmup1.ejs");
 	this.addStaticScreen("warmup2.ejs");
 	this.addStaticScreen("warmup3.ejs");
 	this.addStaticScreen("warmup4.ejs");
