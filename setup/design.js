655d6988f4068828cc9efa11ce90c7a5b4348005
Anna Laurinavichyute <annlaurin@gmail.com>
2014-10-31 18:19:10 +0300

    trying to fix the stupid presentation

--- setup/design.js
+++ setup/design.js
@@ -7,11 +7,11 @@ Experigen.initialize = function () {
 
 	this.addStaticScreen("intro.ejs");
 	this.addStaticScreen("concent.ejs");
-//	this.addStaticScreen("warmup.ejs");
-//	this.addStaticScreen("warmup1.ejs");
-//	this.addStaticScreen("warmup2.ejs");
-//	this.addStaticScreen("warmup3.ejs");
-//	this.addStaticScreen("warmup4.ejs");
+	this.addStaticScreen("warmup.ejs");
+	this.addStaticScreen("warmup1.ejs");
+	this.addStaticScreen("warmup2.ejs");
+	this.addStaticScreen("warmup3.ejs");
+	this.addStaticScreen("warmup4.ejs");
 	this.addStaticScreen("getgoing.ejs")
 	this.addBlock(tests);
 	this.addStaticScreen("demographic.ejs");
